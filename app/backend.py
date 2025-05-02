85229fc962c392036ede6e2c74f2aba75e22f7ec
dmmx <bdh61@me.com>
2025-05-02 15:12:40 -0500

    update backend with new hostname, front end fix.

--- app/backend.py
+++ app/backend.py
@@ -29,4 +29,4 @@ def verify():
 
 if __name__ == "__main__":
     # app.run(debug=True, ssl_context='adhoc')
-    app.run(debug=True)
+    app.run(debug=True, host='ec2-3-145-156-203.us-east-2.compute.amazonaws.com')
